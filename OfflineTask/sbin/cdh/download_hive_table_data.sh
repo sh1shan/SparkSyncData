459d7d3995b238010febcf572e8c43194d1663d5
shishan <cclin578089476@gmail.com>
2021-06-12 11:33:36 +0800

    添加spark执行参数

--- OfflineTask/sbin/cdh/download_hive_table_data.sh
+++ OfflineTask/sbin/cdh/download_hive_table_data.sh
@@ -2,3 +2,25 @@
 
 DATE=`data -d "$1 -2 days" +%Y%m%d`
 YESTERDAY=`date -d "$1 -3 days"+%Y%m%d`
+SPARK='SPARK_CLUSTER'
+
+MALBEL2CUTOMBYDAY(){
+    $SPARK \
+    --class you_java_class_path \
+    --master yarn-cluster \
+    --num-excutors 60 \
+    --queue ssyx \
+    --driver-memory 10g \
+    --excutor-cores 2 \
+    --conf spark.yarn.excutor.memmoryOverhead=4096 \
+    --conf spark.network.timeout=500s \
+    --conf spark.shuffle.io.maxRetries=12 \
+    --conf spark.shuffle.io.retryWait=60s \
+    --conf spark.default.parallelism=240 \
+    --conf spark.excutor.heartbeatInterval=20s \
+    --conf spark.default.parallelism=200 \
+    ${JAR_PATH} ${ENV} ${IMPORT_HDFS_DIR}/${DATA_FILE} ${DATE} 100 1
+
+
+}
+MALBEL2CUTOMBYDAY
